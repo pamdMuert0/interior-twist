cddeb7684aefa34b9113241659544ca395efe7ba
fungirox <annxrk13@gmail.com>
2024-10-18 19:24:11 -0700

    first commit

---
+++ Assets/UIButtons.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIButtons : MonoBehaviour
+{
+    public GameObject pnPause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)){
+            pnPause.SetActive(true);
+        }
+
+    }
+    public void exit(){
+        Application.Quit();
+    }
+    public void loadScene(string scene){
+        SceneManager.LoadScene(scene);
+    }
+    public void closePanel(){
+        pnPause.SetActive(false);
+    }
+
+}
